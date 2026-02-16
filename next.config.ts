44b4de7d84f6f41a99ccd11c020c6286817ee56c
JumpyJay <iuswtric1909@gmail.com>
2026-02-17 01:35:10 +0700

    set up cicd, using github actions, and containerised with docker

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export", // This creates the 'out' folder during build
+  images: {
+    unoptimized: true, // Necessary for static export unless using a custom loader
+  },
 };
 
 export default nextConfig;
